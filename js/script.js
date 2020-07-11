5bc6c0b5a63c6c7e2063924f0c0d83a6aa94dada
End-front <62587226+End-front@users.noreply.github.com>
2020-07-11 16:56:42 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -294,7 +294,7 @@ $(document).ready(function() {
   let scene5_0 = new ScrollMagic.Scene({
     triggerElement: '#section-3',
     offset: (-headerHeight - 20 + PaddingScene5),
-    duration: (-headerHeight - 20 + PaddingScene5) + standartDuration * 7 + PauseBetweenScene * 5 + PaddingScene5 * 3 * 1.5 + ((PaddingScene7 + PaddingScene5) * 1.5) * 2,
+    duration: (-headerHeight - 20 + PaddingScene5) + standartDuration * 8 + PauseBetweenScene * 5 + PaddingScene5 * 3 * 1.5 + ((PaddingScene7 + PaddingScene5) * 1.5) * 2,
   })
   .setPin('#section-3')
   .triggerHook(0)
